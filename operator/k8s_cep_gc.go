bpourriahi/cilium

commit 37139c33fb791d375ca852a01e176b603810cd54

operator: remove pod list of an entire cluster
Doing a pod list every 30 minutes can be brutal for kube-apiserver,
specially if it is not running with a limit of the number of pods that
should be returned. Having a watcher that receives notifications for the
pods running as well as the CEPs, is enough to provide a GC for
Cilium Identities and CEPs.

Since we have a k8s watcher and the CEPs have an owner set to
themselves the CEP GC interval was decreased to 5 minutes.

Also, given that we don't depend on the CEP GC to perform Cilium Identity
GC we can set the identity heartbeat timeout to 2* the KVStore Lease TTL.

Signed-off-by: André Martins <andre@cilium.io>

operator/k8s_cep_gc.go
@@ -16,17 +16,15 @@ package main
 
 import (
 	"context"
-	"strconv"
-	"time"
 
 	operatorOption "github.com/cilium/cilium/operator/option"
+	"github.com/cilium/cilium/operator/watchers"
 	"github.com/cilium/cilium/pkg/controller"
 	"github.com/cilium/cilium/pkg/k8s"
+	cilium_v2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 
 	"github.com/sirupsen/logrus"
-	core_v1 "k8s.io/api/core/v1"
-	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -35,9 +33,7 @@ import (
 // objects are created by the sync-to-k8s-ciliumendpoint controller on each
 // Endpoint.
 // The general steps are:
-//   - get list of nodes
-//   - only run with probability 1/nodes
-//   - get list of CEPs
+//   - list all CEPs in the cluster
 //   - for each CEP
 //       delete CEP if the corresponding pod does not exist
 // CiliumEndpoint objects have the same name as the pod they represent
@@ -51,81 +47,50 @@ func enableCiliumEndpointSyncGC() {
 
 	ciliumClient := ciliumK8sClient.CiliumV2()
 
+	// This functions will block until the resources are synced with k8s.
+	watchers.CiliumEndpointsInit(ciliumClient)
+	watchers.PodsInit(k8s.WatcherCli())
+
 	// this dummy manager is needed only to add this controller to the global list
 	controller.NewManager().UpdateController(controllerName,
 		controller.ControllerParams{
 			RunInterval: operatorOption.Config.EndpointGCInterval,
 			DoFunc: func(ctx context.Context) error {
-				var (
-					listOpts = meta_v1.ListOptions{Limit: 10}
-					loopStop = time.Now().Add(operatorOption.Config.EndpointGCInterval)
-				)
+				// For each CEP we fetched, check if we know about it
+				for _, cepObj := range watchers.CiliumEndpointStore.List() {
+					cep, ok := cepObj.(*cilium_v2.CiliumEndpoint)
+					if !ok {
+						log.WithField(logfields.Object, cepObj).
+							Errorf("Saw %T object while expecting *cilium_v2.CiliumEndpoint", cepObj)
+						continue
+					}
 
-				pods, err := k8s.Client().CoreV1().Pods("").List(ctx, meta_v1.ListOptions{})
-				if err != nil {
-					return err
-				}
-
-				podsCache := map[string]*core_v1.Pod{}
-				for _, pod := range pods.Items {
-					podsCache[pod.Namespace+"/"+pod.Name] = &pod
-				}
-
-				timeNow := time.Now()
-			perCEPFetch:
-				for time.Now().Before(loopStop) { // Guard against no-break bugs
-					time.Sleep(time.Second) // Throttle lookups in case of a busy loop
-
-					ceps, err := ciliumClient.CiliumEndpoints(meta_v1.NamespaceAll).List(ctx, listOpts)
-					switch {
-					case err != nil && k8serrors.IsResourceExpired(err) && ceps.Continue != "":
-						// This combination means we saw a 410 ResourceExpired error but we
-						// can iterate on the now-current snapshot. We need to refetch,
-						// however.
-						// See https://github.com/kubernetes/apimachinery/blob/master/pkg/apis/meta/v1/types.go#L350-L381
-						// or the docs for k8s.io/apimachinery/pkg/apis/meta/v1.ListOptions
-						// vendored into this repo.
-						listOpts.Continue = ceps.Continue
-						continue perCEPFetch
-
-					case err != nil:
-						scopedLog.WithError(err).Debug("Cannot list CEPs")
+					cepFullName := cep.Namespace + "/" + cep.Name
+					_, exists, err := watchers.PodStore.GetByKey(cepFullName)
+					if err != nil {
+						scopedLog.WithError(err).Warn("Unable to get pod from store")
+						continue
+					}
+					if !exists {
+						// FIXME: this is fragile as we might have received the
+						// CEP notification first but not the pod notification
+						// so we need to have a similar mechanism that we have
+						// for the keep alive of security identities.
+						scopedLog = scopedLog.WithFields(logrus.Fields{
+							logfields.EndpointID: cep.Status.ID,
+							logfields.K8sPodName: cepFullName,
+						})
+						scopedLog.Debug("Orphaned CiliumEndpoint is being garbage collected")
+						PropagationPolicy := meta_v1.DeletePropagationBackground // because these are const strings but the API wants pointers
+						err := ciliumClient.CiliumEndpoints(cep.Namespace).Delete(
+							ctx,
+							cep.Name,
+							meta_v1.DeleteOptions{PropagationPolicy: &PropagationPolicy})
+						if err != nil {
+							scopedLog.WithError(err).Debug("Unable to delete orphaned CEP")
+						}
 						return err
 					}
-
-					// setup listOpts for the next iteration
-					listOpts.Continue = ceps.Continue
-
-					// For each CEP we fetched, check if we know about it
-					for _, cep := range ceps.Items {
-						if cep.Status.Identity != nil {
-							identityHeartbeat.MarkAlive(strconv.FormatInt(cep.Status.Identity.ID, 10), timeNow)
-						}
-
-						cepFullName := cep.Namespace + "/" + cep.Name
-						_, exists := podsCache[cepFullName]
-						if !exists {
-							// delete
-							scopedLog = scopedLog.WithFields(logrus.Fields{
-								logfields.EndpointID: cep.Status.ID,
-								logfields.K8sPodName: cepFullName,
-							})
-							scopedLog.Debug("Orphaned CiliumEndpoint is being garbage collected")
-							PropagationPolicy := meta_v1.DeletePropagationBackground // because these are const strings but the API wants pointers
-							if err := ciliumClient.CiliumEndpoints(cep.Namespace).Delete(
-								ctx,
-								cep.Name,
-								meta_v1.DeleteOptions{PropagationPolicy: &PropagationPolicy}); err != nil {
-								scopedLog.WithError(err).Debug("Unable to delete orphaned CEP")
-								return err
-							}
-						}
-					}
-					if ceps.Continue != "" {
-						// there is more data, continue
-						continue perCEPFetch
-					}
-					break perCEPFetch // break out as a safe default to avoid spammy loops
 				}
 				return nil
 			},

operator/flags.go
@@ -131,7 +131,7 @@ func init() {
 	option.BindEnv(operatorOption.EnableCEPGC)
 	flags.MarkDeprecated(operatorOption.EnableCEPGC, fmt.Sprintf("Please use %s=0 to disable CEP GC", operatorOption.EndpointGCInterval))
 
-	flags.Duration(operatorOption.EndpointGCInterval, 10*time.Minute, "GC interval for cilium endpoints")
+	flags.Duration(operatorOption.EndpointGCInterval, operatorOption.EndpointGCIntervalDefault, "GC interval for cilium endpoints")
 	option.BindEnv(operatorOption.EndpointGCInterval)
 
 	flags.Bool(operatorOption.EnableMetrics, false, "Enable Prometheus metrics")
@@ -140,8 +140,7 @@ func init() {
 	flags.String(option.IPAM, option.IPAMHostScopeLegacy, "Backend to use for IPAM")
 	option.BindEnv(option.IPAM)
 
-	// 31 minutes is the default to be slightly larger than 3x EndpointGCInterval
-	flags.Duration(operatorOption.IdentityHeartbeatTimeout, 31*time.Minute, "Timeout after which identity expires on lack of heartbeat")
+	flags.Duration(operatorOption.IdentityHeartbeatTimeout, 2*defaults.KVstoreLeaseTTL, "Timeout after which identity expires on lack of heartbeat")
 	option.BindEnv(operatorOption.IdentityHeartbeatTimeout)
 
 	flags.Bool(option.EnableIPv4Name, defaults.EnableIPv4, "Enable IPv4 support")

operator/k8s_identity.go
@@ -20,6 +20,7 @@ import (
 
 	"github.com/cilium/cilium/operator/identity"
 	operatorOption "github.com/cilium/cilium/operator/option"
+	"github.com/cilium/cilium/operator/watchers"
 	"github.com/cilium/cilium/pkg/controller"
 	"github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
 	"github.com/cilium/cilium/pkg/k8s/informer"
@@ -62,14 +63,20 @@ var identityHeartbeat *identity.IdentityHeartbeatStore
 // identityGCIteration is a single iteration of a garbage collection. It will
 // delete identities that have not had its heartbeat lifesign updated since
 // option.Config.IdentityHeartbeatTimeout
-func identityGCIteration() {
+func identityGCIteration(ctx context.Context) {
 	log.Debug("Running CRD identity garbage collector")
 
 	if identityStore == nil {
 		log.Debug("Identity store cache is not ready yet")
 		return
 	}
+	select {
+	case <-watchers.CiliumEndpointsSynced:
+	case <-ctx.Done():
+		return
+	}
 
+	timeNow := time.Now()
 	for _, identityObject := range identityStore.List() {
 		identity, ok := identityObject.(*types.Identity)
 		if !ok {
@@ -78,6 +85,12 @@ func identityGCIteration() {
 			continue
 		}
 
+		// The identity is definitely alive if there's a CE using it.
+		if watchers.HasCEWithIdentity(identity.Name) {
+			// If the identity is alive then mark it as alive
+			identityHeartbeat.MarkAlive(identity.Name, timeNow)
+			continue
+		}
 		if !identityHeartbeat.IsAlive(identity.Name) {
 			log.WithFields(logrus.Fields{
 				logfields.Identity: identity,
@@ -92,7 +105,7 @@ func identityGCIteration() {
 
 func startCRDIdentityGC() {
 	if operatorOption.Config.EndpointGCInterval == 0 {
-		log.Fatal("The CiliumIdentity garabge collector requires the CiliumEndpoint garbage collector to be enabled")
+		log.Fatal("The CiliumIdentity garbage collector requires the CiliumEndpoint garbage collector to be enabled")
 	}
 
 	log.Infof("Starting CRD identity garbage collector with %s interval...", operatorOption.Config.IdentityGCInterval)
@@ -101,8 +114,8 @@ func startCRDIdentityGC() {
 		controller.ControllerParams{
 			RunInterval: operatorOption.Config.IdentityGCInterval,
 			DoFunc: func(ctx context.Context) error {
-				identityGCIteration()
-				return nil
+				identityGCIteration(ctx)
+				return ctx.Err()
 			},
 		})
 }

operator/option/config.go
@@ -21,6 +21,11 @@ import (
 )
 
 const (
+	// EndpointGCIntervalDefault is the default time for the CEP GC
+	EndpointGCIntervalDefault = 5 * time.Minute
+)
+
+const (
 	// CNPNodeStatusGCInterval is the GC interval for nodes which have been
 	// removed from the cluster in CiliumNetworkPolicy and
 	// CiliumClusterwideNetworkPolicy Status.

operator/watchers/cilium_endpoint.go
@@ -1,0 +1,139 @@
+// Copyright 2016-2020 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package watchers
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+
+	cilium_api_v2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
+	cilium_cli "github.com/cilium/cilium/pkg/k8s/client/clientset/versioned/typed/cilium.io/v2"
+	"github.com/cilium/cilium/pkg/k8s/informer"
+
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/tools/cache"
+)
+
+const identityIndex = "identity"
+
+var (
+	errNoCE  = errors.New("object is not a *cilium_api_v2.CiliumEndpoint")
+	indexers = cache.Indexers{
+		cache.NamespaceIndex: cache.MetaNamespaceIndexFunc,
+		identityIndex:        identityIndexFunc,
+	}
+
+	// CiliumEndpointStore contains all CiliumEndpoint present in k8s.
+	// Warning: The CiliumEndpoints stored in the cache are not intended to be
+	// used for Update operations in k8s as some of its fields were are not
+	// populated.
+	CiliumEndpointStore cache.Indexer
+
+	// CiliumEndpointsSynced is closed once the CiliumEndpointStore is synced
+	// with k8s.
+	CiliumEndpointsSynced = make(chan struct{})
+)
+
+// identityIndexFunc index identities by ID.
+func identityIndexFunc(obj interface{}) ([]string, error) {
+	switch t := obj.(type) {
+	case *cilium_api_v2.CiliumEndpoint:
+		if t.Status.Identity != nil {
+			id := strconv.FormatInt(t.Status.Identity.ID, 10)
+			return []string{id}, nil
+		}
+		return []string{"0"}, nil
+	}
+	return nil, fmt.Errorf("%w - found %T", errNoCE, obj)
+}
+
+// CiliumEndpointsInit starts a CiliumEndpointWatcher
+func CiliumEndpointsInit(ciliumNPClient cilium_cli.CiliumV2Interface) {
+	CiliumEndpointStore = cache.NewIndexer(cache.DeletionHandlingMetaNamespaceKeyFunc, indexers)
+
+	ciliumEndpointInformer := informer.NewInformerWithStore(
+		cache.NewListWatchFromClient(ciliumNPClient.RESTClient(),
+			"ciliumendpoints", v1.NamespaceAll, fields.Everything()),
+		&cilium_api_v2.CiliumEndpoint{},
+		0,
+		cache.ResourceEventHandlerFuncs{},
+		convertToCiliumEndpoint,
+		CiliumEndpointStore,
+	)
+	go ciliumEndpointInformer.Run(wait.NeverStop)
+
+	cache.WaitForCacheSync(wait.NeverStop, ciliumEndpointInformer.HasSynced)
+	close(CiliumEndpointsSynced)
+}
+
+// convertToCiliumEndpoint converts a CiliumEndpoint to a minimal CiliumEndpoint
+// containing only a minimal set of entities used to identity a CiliumEndpoint
+// Warning: The CiliumEndpoints created by the converter are not intended to be
+// used for Update operations in k8s.
+func convertToCiliumEndpoint(obj interface{}) interface{} {
+	switch concreteObj := obj.(type) {
+	case *cilium_api_v2.CiliumEndpoint:
+		p := &cilium_api_v2.CiliumEndpoint{
+			TypeMeta: concreteObj.TypeMeta,
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      concreteObj.Name,
+				Namespace: concreteObj.Namespace,
+			},
+			Status: cilium_api_v2.EndpointStatus{
+				Identity: concreteObj.Status.Identity,
+			},
+		}
+		*concreteObj = cilium_api_v2.CiliumEndpoint{}
+		return p
+	case cache.DeletedFinalStateUnknown:
+		ciliumEndpoint, ok := concreteObj.Obj.(*cilium_api_v2.CiliumEndpoint)
+		if !ok {
+			return obj
+		}
+		dfsu := cache.DeletedFinalStateUnknown{
+			Key: concreteObj.Key,
+			Obj: &cilium_api_v2.CiliumEndpoint{
+				TypeMeta: ciliumEndpoint.TypeMeta,
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      ciliumEndpoint.Name,
+					Namespace: ciliumEndpoint.Namespace,
+				},
+				Status: cilium_api_v2.EndpointStatus{
+					Identity: ciliumEndpoint.Status.Identity,
+				},
+			},
+		}
+		// Small GC optimization
+		*ciliumEndpoint = cilium_api_v2.CiliumEndpoint{}
+		return dfsu
+	default:
+		return obj
+	}
+}
+
+// HasCEWithIdentity returns true or false if the Cilium Endpoint store has
+// the given identity.
+func HasCEWithIdentity(identity string) bool {
+	if CiliumEndpointStore == nil {
+		return false
+	}
+	ces, _ := CiliumEndpointStore.IndexKeys(identityIndex, identity)
+
+	return len(ces) != 0
+}

operator/watchers/pod.go
@@ -1,0 +1,91 @@
+// Copyright 2016-2020 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package watchers
+
+import (
+	"github.com/cilium/cilium/pkg/k8s/informer"
+	slim_corev1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/apis/core/v1"
+	slim_metav1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/apis/meta/v1"
+
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/cache"
+)
+
+var (
+	// PodStore has a minimal copy of all pods running in the cluster.
+	// Warning: The pods stored in the cache are not intended to be used for Update
+	// operations in k8s as some of its fields were are not populated.
+	PodStore cache.Store
+
+	// PodStoreSynced is closed once the PodStore is synced with k8s.
+	PodStoreSynced = make(chan struct{})
+)
+
+func PodsInit(k8sClient kubernetes.Interface) {
+	var podInformer cache.Controller
+	PodStore, podInformer = informer.NewInformer(
+		cache.NewListWatchFromClient(k8sClient.CoreV1().RESTClient(),
+			"pods", v1.NamespaceAll, fields.Everything()),
+		&slim_corev1.Pod{},
+		0,
+		cache.ResourceEventHandlerFuncs{},
+		convertToPod,
+	)
+	go podInformer.Run(wait.NeverStop)
+
+	cache.WaitForCacheSync(wait.NeverStop, podInformer.HasSynced)
+	close(PodStoreSynced)
+}
+
+// convertToPod stores a minimal version of the pod as it is only intended
+// for it to check if a pod is running in the cluster or not. The stored pod
+// should not be used to update an existing pod in the kubernetes cluster.
+func convertToPod(obj interface{}) interface{} {
+	switch concreteObj := obj.(type) {
+	case *slim_corev1.Pod:
+		p := &slim_corev1.Pod{
+			TypeMeta: concreteObj.TypeMeta,
+			ObjectMeta: slim_metav1.ObjectMeta{
+				Name:      concreteObj.Name,
+				Namespace: concreteObj.Namespace,
+			},
+		}
+		*concreteObj = slim_corev1.Pod{}
+		return p
+	case cache.DeletedFinalStateUnknown:
+		pod, ok := concreteObj.Obj.(*slim_corev1.Pod)
+		if !ok {
+			return obj
+		}
+		dfsu := cache.DeletedFinalStateUnknown{
+			Key: concreteObj.Key,
+			Obj: &slim_corev1.Pod{
+				TypeMeta: pod.TypeMeta,
+				ObjectMeta: slim_metav1.ObjectMeta{
+					Name:      pod.Name,
+					Namespace: pod.Namespace,
+				},
+			},
+		}
+		// Small GC optimization
+		*pod = slim_corev1.Pod{}
+		return dfsu
+	default:
+		return obj
+	}
+}

commit 70934d326955bde7de63d3553ebe1be165a98b45

operator: only do earlier return on error condition of GCed CEPs
Doing a return regardless of the error condition would prevent Cilium
Endpoints to be GCed in a single run.

Fixes: 37139c33fb79 ("operator: remove pod list of an entire cluster")
Signed-off-by: André Martins <andre@cilium.io>

operator/k8s_cep_gc.go
@@ -89,8 +89,8 @@ func enableCiliumEndpointSyncGC() {
 							meta_v1.DeleteOptions{PropagationPolicy: &PropagationPolicy})
 						if !k8serrors.IsNotFound(err) {
 							scopedLog.WithError(err).Warning("Unable to delete orphaned CEP")
+							return err
 						}
-						return err
 					}
 				}
 				return nil